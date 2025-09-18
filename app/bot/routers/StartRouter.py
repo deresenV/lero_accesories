6a1560dbc2750f5a59e0af4048400ebc8396a4d1
deresenV <deresenvil@gmail.com>
2025-09-19 01:01:27 +0300

    create user

--- app/bot/routers/StartRouter.py
+++ app/bot/routers/StartRouter.py
@@ -3,6 +3,7 @@ from aiogram.filters import Command
 
 from app.bot.keyboards.StartKeyboard import start_keyboard
 from app.bot.routers.BaseRouter import BaseRouter
+from app.services.UserService import UserService
 
 
 class StartRouter(BaseRouter):
@@ -13,5 +14,8 @@ class StartRouter(BaseRouter):
         self.router.message(Command("start"))(self.start_command)
 
 
-    async def start_command(self, message: Message):
+    async def start_command(self,
+                            message: Message,
+                            user_service: UserService):
+        user = await user_service.get_or_create_user(message.from_user.id)
         await message.answer("Воспользуйтесь меню для продолжения", reply_markup=start_keyboard)
