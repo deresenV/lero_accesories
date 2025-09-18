20c10d4c01d0545838411ef92fc73bb5d896547e
deresenV <deresenvil@gmail.com>
2025-09-19 01:02:07 +0300

    update middleware

--- app/bot/main.py
+++ app/bot/main.py
@@ -1,10 +1,11 @@
 import asyncio
 
+from app.bot.middlewares.services import ServiceMiddleware
 from app.bot.routers import routers
 from app.config import settings
 from aiogram import Bot, Dispatcher
 
-from app.db.database import create_tables
+from app.db.database import create_tables, AsyncSessionLocal
 
 dp = Dispatcher()
 
@@ -12,6 +13,7 @@ async def main() -> None:
     await create_tables()
     bot = Bot(token=settings.bot_token)
     dp.include_router(routers)
+    dp.update.middleware(ServiceMiddleware(AsyncSessionLocal))
     await dp.start_polling(bot)
 
 
