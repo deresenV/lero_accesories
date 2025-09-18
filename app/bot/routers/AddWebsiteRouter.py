05f562f9b348bcc73501c0afb7e2aa2eb8fc0ed1
deresenV <deresenvil@gmail.com>
2025-09-19 01:23:21 +0300

    add cancel keyboard

--- app/bot/routers/AddWebsiteRouter.py
+++ app/bot/routers/AddWebsiteRouter.py
@@ -1,10 +1,11 @@
-from aiogram import F
-from aiogram.types import Message, ReplyKeyboardRemove
-from aiogram.fsm.context import FSMContext
-
+from app.bot.keyboards.CancelKeyboard import cancel_kb
+from app.bot.keyboards.StartKeyboard import start_keyboard
 from app.bot.routers.BaseRouter import BaseRouter
 from app.bot.states.AddWebsiteState import AddSite
 
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram import F
 
 class AddWebSiteRouter(BaseRouter):
     def __init__(self):
@@ -15,27 +16,39 @@ class AddWebSiteRouter(BaseRouter):
         self.router.message(AddSite.waiting_for_url)(self.process_url)
         self.router.message(AddSite.waiting_for_interval)(self.process_interval)
 
-
     async def add_site_start_message(self, message: Message, state: FSMContext):
-        await message.answer("Следующим сообщением отправьте URL веб-сайта:", reply_markup=ReplyKeyboardRemove())
+        await message.answer("Следующим сообщением отправьте URL веб-сайта:", reply_markup=cancel_kb)
         await state.set_state(AddSite.waiting_for_url)
 
 
     async def process_url(self,message: Message, state: FSMContext):
-        await state.update_data(url=message.text)
-        await message.answer("Введите интервал проверки (в минутах):")
+        url = message.text
+        if url == "Отмена":
+            await state.clear()
+            await message.answer("Отменено", reply_markup=start_keyboard)
+            return
+
+
+        await state.update_data(url=url)
+        await message.answer("Введите интервал проверки (в минутах):", reply_markup=cancel_kb)
         await state.set_state(AddSite.waiting_for_interval)
 
 
     async def process_interval(self,message: Message, state: FSMContext):
         interval = message.text
-
-        if not message.text.isnumeric():
-            await message.answer("Время должно быть указано числом!")
+        if interval == "Отмена":
+            await state.clear()
+            await message.answer("Отменено", reply_markup=start_keyboard)
             return
 
-        if int(interval)<=0:
-            await message.answer("Время должно быть > 0")
+        try:
+            interval = int(message.text)
+        except ValueError:
+            await message.answer("Время должно быть указано числом!", reply_markup=cancel_kb)
+            return
+
+        if interval <= 0:
+            await message.answer("Время должно быть > 0", reply_markup=cancel_kb)
             return
 
         data = await state.get_data()
