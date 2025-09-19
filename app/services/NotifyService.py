4b6e2d37c69daa0be3b6cdecd5bfb38807e38ceb
deresenV <deresenvil@gmail.com>
2025-09-19 21:28:53 +0300

    init NotifyService.py

--- app/services/NotifyService.py
+++ app/services/NotifyService.py
@@ -1,10 +1,6 @@
-from aiogram import Bot
-
-
 class NotifyService:
-    def __init__(self, bot: Bot):
+    def __init__(self, bot):
         self.bot = bot
 
-
-    async def send_msg(self, site):
-        await self.bot.send_message(chat_id=int(site.telegram_id_author),text=f"Проверен {site.url}")
+    async def send_message(self, site, message):
+        await self.bot.send_message(chat_id=int(site.telegram_id_author),text=message)
