ab3991142ff8b741d784e91527f57a866c2b739a
deresenV <deresenvil@gmail.com>
2025-09-19 17:44:20 +0300

    Подключение фоновых задач опроса сервера

--- app/bot/main.py
+++ app/bot/main.py
@@ -1,11 +1,15 @@
 import asyncio
-
-from app.bot.middlewares.services import ServiceMiddleware
-from app.bot.routers import routers
-from app.config import settings
 from aiogram import Bot, Dispatcher
 
+from app.config import settings
+from app.bot.routers import routers
+from app.bot.middlewares.services import ServiceMiddleware
 from app.db.database import create_tables, AsyncSessionLocal
+from app.db.repositories.SiteRepository import SiteRepository
+from app.monitoring.BackgroundTask import BackgroundTask
+from app.monitoring.SiteChecker import SiteChecker
+from app.services.NotifyService import NotifyService
+from app.services.SiteService import SiteService
 
 dp = Dispatcher()
 
@@ -13,9 +17,25 @@ async def main() -> None:
     await create_tables()
     bot = Bot(token=settings.bot_token)
     dp.include_router(routers)
-    dp.update.middleware(ServiceMiddleware(AsyncSessionLocal))
-    await dp.start_polling(bot)
 
+    checker = BackgroundTask()
+    notify_service = NotifyService(bot=bot)
+    site_checker = SiteChecker()
+    async with AsyncSessionLocal() as session:
+        site_repo = SiteRepository(session)
+        site_service = SiteService(site_repo, checker)
+        checker.set_service(site_service)
+        checker.notify_service = notify_service
+        checker.site_checker = site_checker
+
+
+
+    dp.update.middleware(ServiceMiddleware(AsyncSessionLocal, checker))
+
+    await checker.load_sites()
+    asyncio.create_task(checker.run())
+
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
