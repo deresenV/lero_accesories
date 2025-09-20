e6233f0308715a42ea9e495582e77b7c5f68aa66
deresenV <deresenvil@gmail.com>
2025-09-20 14:42:36 +0300

    Добавлены клавиатуры для удобной работы в сайтах пользователя

--- app/bot/keyboards/SiteKeyboards.py
+++ app/bot/keyboards/SiteKeyboards.py
@@ -3,7 +3,10 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardBut
 def site_info_keyboard(site_id: int) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="История", callback_data=f"history_site:{site_id}")],
+            [
+                InlineKeyboardButton(text="История", callback_data=f"history_site:{site_id}"),
+                InlineKeyboardButton(text="Статистика", callback_data=f"site_stats:{site_id}")
+            ],
             [
                 InlineKeyboardButton(text="Редактировать", callback_data=f"edit_site:{site_id}"),
                 InlineKeyboardButton(text="Удалить", callback_data=f"delete_site:{site_id}")
@@ -29,6 +32,27 @@ def remove_site_keyboard(site_id: int) -> InlineKeyboardMarkup:
         inline_keyboard=[
             [InlineKeyboardButton(text="Удалить", callback_data=f"delete_site_succesful:{site_id}")],
             [InlineKeyboardButton(text="Отмена", callback_data=f"site:{site_id}")]
+        ]
+    )
+    return keyboard
+
+def history_keyboard(site_id: int) -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text="Назад", callback_data=f"site:{site_id}")
+            ],
+            [
+                InlineKeyboardButton(text="Скачать полный лог", callback_data=f"download_log:{site_id}")
+            ]
+        ]
+    )
+    return keyboard
+
+def back_statistic_keyboard(site_id: int) -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Назад", callback_data=f"site:{site_id}")]
         ]
     )
     return keyboard
