d9980d95c3f4bac610b9ebb22ff26d2fb10949e6
deresenV <deresenvil@gmail.com>
2025-09-20 14:44:26 +0300

    Логика работы с фоновыми задачами

--- app/monitoring/BackgroundTask.py
+++ app/monitoring/BackgroundTask.py
@@ -10,7 +10,9 @@ class BackgroundTask:
         from app.monitoring.SiteChecker import SiteChecker
         from app.services.NotifyService import NotifyService
         from app.services.SiteService import SiteService
+
         self.sites_schedule = {}
+
         self.site_service: Optional[SiteService] = None
         self.notify_service: Optional[NotifyService] = None
         self.site_checker: Optional[SiteChecker] = None
@@ -27,30 +29,43 @@ class BackgroundTask:
         for site in sites:
             self.sites_schedule[site.id] = {
                 "site": site,
+                "last_response": None,
                 "next_check": now + timedelta(minutes=site.check_interval)
             }
 
 
-    async def add_or_update_site(self, site):
+    async def add_or_update_site(self, site, last_response = None):
         """Добавление нового сайта или обновление интервала существующего"""
         now = datetime.utcnow()
         self.sites_schedule[site.id] = {
             "site": site,
+            "last_response": last_response,
             "next_check": now + timedelta(minutes=site.check_interval)
         }
 
+    async def delete_site(self, site_id: int):
+        """Удаляем сайт из расписания безопасно"""
+        if site_id in self.sites_schedule:
+            self.sites_schedule.pop(site_id)
 
-    async def delete_site(self, site):
-        ...
-        #TODO
 
-    async def processing_site(self, site):
+    async def processing_site(self, site, last_response):
         """Обработка сайта"""
         status_code, response_time, content = await self.site_checker.check_site(site.url)
         if not status_code:
-            await self.notify_service.send_message(site, f"Возникла ошибка при обработке {site.url}.\nПроверьте правильность URL адресса")
-        elif status_code>=500:
-            await self.notify_service.send_message(site, f"Сайт: {site.url}\nВернул: {status_code} код.")
+            if last_response != "error":
+                await self.notify_service.send_message(site.telegram_id_author, f"Возникла ошибка при обработке {site.url}.\nПроверьте правильность URL адресса")
+                await self.add_or_update_site(site, "error")
+
+        elif status_code>=400:
+            if last_response != "error":
+                await self.notify_service.send_message(site.telegram_id_author, f"Сайт: {site.url}\nВернул: {status_code} код.")
+                await self.add_or_update_site(site, "error")
+
+        else:
+            if last_response:
+                await self.add_or_update_site(site, None)
+                await self.notify_service.send_message(site.telegram_id_author, f"Сайт {site.url} стал вновь доступен!")
 
         await self.logs_service.create_site_log(site, status_code, response_time)
 
@@ -61,6 +76,6 @@ class BackgroundTask:
             now = datetime.utcnow()
             for site_id, info in list(self.sites_schedule.items()):
                 if now >= info["next_check"]:
-                    await self.processing_site(info["site"])
+                    await self.processing_site(info["site"], info["last_response"])
                     info["next_check"] = now + timedelta(minutes=info["site"].check_interval)
             await asyncio.sleep(5)
